422d6ae66a001908341d395abf16ebf8b2b36b60
Ratbyansa Nur <byan@sgo.co.id>
2025-01-02 10:53:01 +0700

    feat(list-campus): added navigation screens & custom toolbar, searchbar and other generic views

---
+++ app/src/main/java/com/byansanur/campuslist/presentation/generic_views/SearchBar.kt
@@ -0,0 +1,119 @@
+package com.byansanur.campuslist.presentation.generic_views
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MySearchBar(
+    modifier : Modifier = Modifier,
+    text : String = "",
+    placeHolder: String = "Search",
+    onTextChange : (String) -> Unit,
+    onSearchClicked : (String) -> Unit
+){
+    var performSearch by remember { mutableStateOf(false) }
+    LaunchedEffect(key1 = performSearch) {
+        if (performSearch) {
+            onSearchClicked(text)
+            performSearch = false // Reset the trigger
+        }
+    }
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .clip(RoundedCornerShape(20.dp))
+            .border(
+                BorderStroke(
+                    1.dp,
+                    SolidColor(Color.White)
+                ),
+                RoundedCornerShape(20.dp)
+            ),
+        contentAlignment = Alignment.Center
+    ) {
+
+        OutlinedTextField(
+            modifier = Modifier
+                .fillMaxWidth()
+                .clip(RoundedCornerShape(20))
+                .height(50.dp),
+            readOnly = false,
+            value = text,
+            onValueChange = {
+                onTextChange(it)
+            },
+            placeholder = {
+                Text(
+                    text = placeHolder,
+                    color = Color.White,
+                    fontSize = 17.sp,
+                    fontWeight = FontWeight.Thin,
+                )
+            },
+            leadingIcon = {
+                IconButton(onClick = { }) {
+                    Icon(
+                        imageVector = Icons.Default.Search,
+                        contentDescription = null,
+                        tint = Color.White,
+                        modifier = Modifier.size(22.dp)
+                    )
+                }
+            },
+            keyboardOptions = KeyboardOptions(
+                imeAction = ImeAction.Search
+            ),
+            keyboardActions = KeyboardActions(
+                onSearch = {
+                    performSearch = true
+                }
+            ),
+            singleLine = true,
+            textStyle = TextStyle(
+                color = Color.White,
+                fontSize = 17.sp,
+                fontWeight = FontWeight.Normal,
+            ),
+            colors = TextFieldDefaults.outlinedTextFieldColors(
+                containerColor = Color.DarkGray,
+                focusedBorderColor = Color.DarkGray,
+                unfocusedBorderColor = Color.White,
+                cursorColor = Color.White
+
+            ),
+        )
+
+    }
+}
