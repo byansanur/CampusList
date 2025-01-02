fd5a7736b70236e09966e39c3f60ef9e697f1897
Ratbyansa Nur <byan@sgo.co.id>
2025-01-02 16:29:35 +0700

    fix(presentation-layer): optimize detail screen with check campus name is not empty

--- app/src/main/java/com/byansanur/campuslist/presentation/campus_screen/detail/CampusDetailScreen.kt
+++ app/src/main/java/com/byansanur/campuslist/presentation/campus_screen/detail/CampusDetailScreen.kt
@@ -3,79 +3,68 @@ package com.byansanur.campuslist.presentation.campus_screen.detail
 import android.annotation.SuppressLint
 import android.graphics.Bitmap
 import android.util.Log
-import android.view.ViewGroup
 import android.webkit.WebSettings
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.runtime.remember
-import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.produceState
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.byansanur.campuslist.R
 import com.byansanur.campuslist.domain.model.Campus
 import com.byansanur.campuslist.presentation.campus_screen.CampusViewModel
-import com.byansanur.campuslist.presentation.campus_screen.search.ListOfSearchCampus
 import com.byansanur.campuslist.presentation.generic_views.CustomToolbar
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.launch
+import com.byansanur.campuslist.presentation.generic_views.ShowDialog
 import kotlinx.coroutines.runBlocking
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 
 
-//@SuppressLint("ProduceStateDoesNotAssignValue")
 @Composable
 internal fun CampusDetailScreen(
     navController: NavController,
     name: String,
     viewModel: CampusViewModel = hiltViewModel()
 ) {
-//    Log.e("TAG", "CampusDetailScreen: id: $name")
-//    val campuses by produceState<Campus>(initialValue = Campus(), key1 = name) {
-//        if (name.isNotEmpty()) {
-//            viewModel.getCampusByName(name).collect { campusList ->
-//                Log.d("TAG", "ListOfSearchCampus: produceState: $campusList")
-//                value = campusList
-//            }
-//        } else {
-//            value = Campus()
-//        }
-//    }
     var campus = Campus()
     runBlocking {
         viewModel.getCampusByName(name).collect {
             campus = it
         }
     }
-    Log.e("TAG", "CampusDetailScreen: detail: ${campus}")
 
-    Scaffold(
-        topBar = {
+    if (campus.name?.isNotEmpty() == true)
+        Column {
             CustomToolbar(
                 campus.name.toString(),
                 navigateBack = { navController.popBackStack() }
             )
-        },
-        content = { paddingValue ->
-            WebViewPage(campus.domains?.get(0).toString(), paddingValue)
-        })
+            WebViewPage(campus.domains?.get(0).toString())
+        }
+    else {
+        ShowDialog(
+            title = stringResource(R.string.oops_sorry),
+            message = stringResource(R.string.something_went_wrong),
+            onClickOk = {
+                navController.popBackStack()
+            }
+        )
+    }
 }
 
 @SuppressLint("SetJavaScriptEnabled")
 @Composable
-fun WebViewPage(domain: String, paddingValues: PaddingValues) {
+fun WebViewPage(domain: String) {
     var isLoading by remember { mutableStateOf(true) }
     val context = LocalContext.current
     val webView = remember {
@@ -95,7 +84,7 @@ fun WebViewPage(domain: String, paddingValues: PaddingValues) {
             settings.cacheMode = WebSettings.LOAD_NO_CACHE
         }
     }
-    Box(modifier = Modifier.fillMaxSize().padding(paddingValues)) {
+    Box(modifier = Modifier.fillMaxSize()) {
         AndroidView(
             modifier = Modifier.fillMaxSize(),
             factory = { webView },
