b7e727d41243093c84fedf730504505d7a1a6d2e
Ratbyansa Nur <byan@sgo.co.id>
2025-01-02 16:15:18 +0700

    fix(domain-layer): fix & add case recent search in CampusRepository.kt

--- app/src/main/java/com/byansanur/campuslist/domain/repository/CampusRepository.kt
+++ app/src/main/java/com/byansanur/campuslist/domain/repository/CampusRepository.kt
@@ -5,5 +5,6 @@ import com.byansanur.campuslist.domain.model.Campus
 interface CampusRepository {
     suspend fun getCampus(refresh: Boolean): List<Campus>
     suspend fun getSearchCampus(name: String, refresh: Boolean): List<Campus>
-    suspend fun getCampusByName(name: String): Campus
+    suspend fun getCampusByName(name: String): Campus?
+    suspend fun getRecentSearch(): List<Campus>
 }
