c2dbb3612bc407acf347b64d200d74d0260162cf
Ratbyansa Nur <byan@sgo.co.id>
2025-01-03 11:24:51 +0700

    test(database): fix provideDatabase with fallbackToDestructiveMigration

--- app/src/main/java/com/byansanur/campuslist/data/local/di/LocalDatabaseModule.kt
+++ app/src/main/java/com/byansanur/campuslist/data/local/di/LocalDatabaseModule.kt
@@ -23,7 +23,7 @@ class LocalDatabaseModule {
             context.applicationContext,
             CampusDatabases::class.java,
             "campus.db"
-        ).build()
+        ).fallbackToDestructiveMigration().build()
     }
 
     @Provides
